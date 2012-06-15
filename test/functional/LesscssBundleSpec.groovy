3a17e1050522a13f65f9614b7b6fdeebbb55e1a0
Gary Turovsky <gary@spantree.net>
2012-06-15 18:19:42 -0500

    Checking in work-in-progress geb spock test of less file recompilation.

--- test/functional/LesscssBundleSpec.groovy
+++ test/functional/LesscssBundleSpec.groovy
@@ -1,8 +1,11 @@
 import geb.spock.GebSpec
 /**
  * @author Paul Fairless
+ * @author Gary Turovsky
  *
  */
+
+import org.apache.commons.io.FileUtils;
 
 class LesscssBundleSpec extends GebSpec {
     def setupSpec() {
@@ -20,5 +23,28 @@ class LesscssBundleSpec extends GebSpec {
 
         and:'css processor still runs'
         $('h1').jquery.css('background-image') =~ '.*/static/images/header-pattern.png.*'
+    }
+
+    def "check on-the-fly compilation"(){
+        File lessFile = new File("web-app/less/test.less")
+        File oldLessFile = new File("web-app/less/test.less.old")
+        File varLessFile = new File("web-app/less/test_variant.less")
+
+        def oldColor, newColor
+
+        when:
+            
+            go('http://localhost:8080/lesscss-resources')
+            oldColor = $('h1').jquery.css('color')
+            FileUtils.copyFile(lessFile, oldLessFile);
+
+            FileUtils.copyFile(varLessFile, lessFile);
+            
+            go('http://localhost:8080/lesscss-resources')
+            newColor = $('h1').jquery.css('color')
+            FileUtils.copyFile(oldLessFile, lessFile);
+
+        then:
+            oldColor != newColor 
     }
 }
